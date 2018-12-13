4208b228ae225a70a34c3311169cb78587df0043
天际 <qrjskyline@gmail.com>
2018-12-13 15:28:30 +0800

    bug fix.

--- src/modules/devops/addMachine.vue
+++ src/modules/devops/addMachine.vue
@@ -3,16 +3,6 @@
         <b-col cols="12" sm="12" md="11" lg="6" xl="6">
             <vue-form :state="formstate" v-model="formstate" @submit.prevent="submit">
                 <validate auto-label class="form-group required-field">
-                    <label>LoginType*</label>
-                    <select class="form-control" name="loginType" required v-model.lazy="machine.loginType">
-                        <option value="password">Password</option>
-                        <option value="privateKey">Key</option>
-                    </select>
-                    <field-messages name="loginType" show="$touched || $dirty || $submitted" class="form-control-feedback">
-                        <div slot="required">Please input content</div>
-                    </field-messages>
-                </validate>
-                <validate auto-label class="form-group required-field">
                     <label>Ip*</label>
                     <input type="text" name="ip" class="form-control" required v-model.lazy="machine.ip">
                     <field-messages name="ip" show="$touched || $submitted" class="form-control-feedback">
@@ -35,25 +25,35 @@
                     </field-messages>
                 </validate>
                 <validate auto-label class="form-group required-field">
-                    <label>LoginPort*</label>
-                    <input type="number" name="loginPort" class="form-control" min="22" max="10000" required v-model.lazy="machine.loginPort">
-                    <field-messages name="loginPort" show="$touched || $submitted" class="form-control-feedback">
-                        <div class="error" slot="required">Please input content</div>
-                        <div class="error" slot="min">the range is between 22 and 10000</div>
-                        <div class="error" slot="max">the range is between 22 and 10000</div>
-                    </field-messages>
-                </validate>
-                <validate auto-label class="form-group required-field">
                     <label>Tags*</label>
                     <input type="text" name="tags" class="form-control" required v-model.lazy="machine.tags">
                     <field-messages name="tags" show="$touched || $submitted" class="form-control-feedback">
                         <div class="error" slot="required">Please input content</div>
                     </field-messages>
                 </validate>
-                
 
-
-
+                <div style="text-align: right"><a href="#" v-on:click="isShowDetail=!isShowDetail">showDetail</a></div>
+                <transition name="fade"><div v-show="isShowDetail">
+                    <validate auto-label class="form-group required-field">
+                        <label>LoginType*</label>
+                        <select class="form-control" name="loginType" required v-model.lazy="machine.loginType">
+                            <option value="password">Password</option>
+                            <option value="privateKey">Key</option>
+                        </select>
+                        <field-messages name="loginType" show="$touched || $dirty || $submitted" class="form-control-feedback">
+                            <div slot="required">Please input content</div>
+                        </field-messages>
+                    </validate>
+                    <validate auto-label class="form-group required-field">
+                        <label>LoginPort*</label>
+                        <input type="number" name="loginPort" class="form-control" min="22" max="10000" required v-model.lazy="machine.loginPort">
+                        <field-messages name="loginPort" show="$touched || $submitted" class="form-control-feedback">
+                            <div class="error" slot="required">Please input content</div>
+                            <div class="error" slot="min">the range is between 22 and 10000</div>
+                            <div class="error" slot="max">the range is between 22 and 10000</div>
+                        </field-messages>
+                    </validate>
+                </div></transition>
 
                 <div class="py-2 text-center">
                     <button class="btn btn-primary" type="submit">Submit</button>
@@ -76,7 +76,8 @@
           loginPort: 22,
           region: ''
         },
-        regions: []
+        regions: [],
+        isShowDetail: false
       }
     },
     methods: {
@@ -107,3 +108,13 @@
     }
   }
 </script>
+
+<style>
+    /*vue animation*/
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity .5s;
+    }
+    .fade-enter, .fade-leave-to {
+        opacity: 0;
+    }
+</style>
